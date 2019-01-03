9e30b60c74bbcc84c87cc11779ac749f9deb3180
EthanID <ethancoea@gmail.com>
2019-01-02 23:46:50 -0500

    added broken code for recent subs

--- background.js
+++ background.js
@@ -1,12 +1,24 @@
+chrome.storage.sync.set({"recentSubs": ["animalz"]}, function() {});
+chrome.storage.sync.set({"favSubs": []}, function() {});
+
 chrome.omnibox.onInputEntered.addListener(
 	function(sub) {
 		chrome.tabs.update({"url": "https://www.reddit.com/r/" + sub});
+
+		chrome.storage.sync.get("recentSubs", function test(localRecentSubs) {
+		  chrome.storage.sync.set({"recentSubs": localRecentSubs + sub}, function() {});
+	  });
 	});
 
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggestions) {
 	  var subs = ["mechanicalkeyboards", "nsfw", "nsfw_gifs", "buildapcsales"];
+	  var subs = [];
 	  var seuggestedSubs = [];
+
+	  chrome.storage.sync.get("recentSubs", function test(localRecentSubs) {
+		  subs = input.recentSubs;
+	  });
 
 	  for(var sub of subs) {
 		  var isMatch = true;
@@ -25,3 +37,4 @@ chrome.omnibox.onInputChanged.addListener(
 
 	  suggestions(seuggestedSubs);
 	});
+
