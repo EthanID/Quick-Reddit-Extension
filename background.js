03b2ecd1c86d6de6af1bcf1202e0484bdec2a70d
EthanID <ethancoea@gmail.com>
2019-01-06 13:10:33 -0500

    added icons and general prettying up

--- background.js
+++ background.js
@@ -2,38 +2,44 @@ chrome.storage.sync.set({"recentSubs": ["animalz"]}, function() {}); // instanti
 chrome.storage.sync.set({"favSubs": []}, function() {});
 
 chrome.omnibox.onInputEntered.addListener(
-	function(sub) {
-		chrome.tabs.update({"url": "https://www.reddit.com/r/" + sub});
+	(sub) => {
 
-		chrome.storage.sync.get("recentSubs", function test(localRecentSubs) {
-	    chrome.storage.sync.set({"recentSubs": localRecentSubs + sub}, function() {});
-	});
+		chrome.storage.sync.get("recentSubs", function(localRecentSubs) {
+			if(sub !== "") {
+				chrome.tabs.update({"url": "https://www.reddit.com/r/" + sub});
+				console.log(localRecentSubs.recentSubs.push(sub));
+				chrome.storage.sync.set({"recentSubs": localRecentSubs.recentSubs.push(sub)});
+			}
+			else {
+				chrome.tabs.update({"url": "https://www.reddit.com"});
+			}
+		});
+
 });
 
 chrome.omnibox.onInputChanged.addListener(
-  function(userSearch, suggestions) {
-	  var subs = ["mechanicalkeyboards", "nsfw", "nsfw_gifs", "buildapcsales"];
-	  var seuggestedSubs = [];
+  (userSearch, suggestions) => {
 
-//	  chrome.storage.sync.get("recentSubs", function test(localRecentSubs) {
-//		  subs = localRecentSubs.recentSubs;
-//	  });
+	  chrome.storage.sync.get("recentSubs", function(localRecentSubs) {
+		  var subs = localRecentSubs.recentSubs;
+		  var seuggestedSubs = [];
 
-	  for(var sub of subs) {
-		  var isMatch = true;
-		  for(var letterNum in userSearch) {
-			  if(userSearch[letterNum] !== sub[letterNum]) {
-				  isMatch = false;
+		  for(var sub of subs) {
+			  var isMatch = true;
+			  for(var letterNum in userSearch) {
+				  if(userSearch[letterNum] !== sub[letterNum]) {
+					  isMatch = false;
+				  }
+			  }
+			  if(isMatch) {
+				  var subObject = new Object();
+				  subObject.content = sub;
+				  subObject.description = sub;
+				  seuggestedSubs.push(subObject);
 			  }
 		  }
-		  if(isMatch) {
-			  var subObject = new Object();
-			  subObject.content = sub;
-			  subObject.description = sub;
-			  seuggestedSubs.push(subObject);
-		  }
-	  }
 
-	  suggestions(seuggestedSubs);
+		  suggestions(seuggestedSubs);
+	  });
+
 	});
-
